79a3bb455a7b5098b7e888f05f06fb65f07ba2f5
RyoLee <516127941@qq.com>
2021-05-18 00:46:24 +0800

    add version file

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
 mkdir -p ./tmp
+mv ./UmaMusumeLibrary.json ./UmaMusumeLibrary.json.old
 rm ./tmp/* -f
 chmod +x ./build.py
 wget https://raw.githubusercontents.com/wrrwrr111/pretty-derby/master/src/assert/db.json -O tmp/db.json
-#wget https://raw.githubusercontents.com/RyoLee/pretty-derby/master/src/assert/cn.json -O tmp/cn.json
+#wget https://raw.githubusercontent.com/RyoLee/pretty-derby/master/src/assert/cn.json -O tmp/cn.json
 ./build.py
+count=$(diff UmaMusumeLibrary.json UmaMusumeLibrary.json.old |wc -l)
+if [ "0"x != "$count"x ];then
+    date +%s > version
+fi
+rm ./UmaMusumeLibrary.json.old
