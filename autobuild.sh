82299ed8abf68e6d801e531f652e382bcf1312f4
RyoLee <516127941@qq.com>
2021-05-18 06:58:29 +0000

    [autobuild] version:1621269958

--- autobuild.sh
+++ autobuild.sh
@@ -12,9 +12,11 @@ if [ "$dbl"x = $odbl"x" ];then
     echo "[info] noupdate"
     exit 0
 fi
+git config --global user.email "516127941@qq.com"
+git config --global user.name "RyoLee"
 git pull
 sh ./build.sh
+echo "$odbl" > ./dbl
 git add ./*
 git commit -m "[autobuild] version:"$(cat version)
 git push
-echo "$odbl" > ./dbl
