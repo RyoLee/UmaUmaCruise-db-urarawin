92111c62b05990ca55cb0a05549596f4a1d11597
RyoLee <516127941@qq.com>
2021-05-20 00:09:51 +0000

    [autobuild] version:1621269958

--- autobuild.sh
+++ autobuild.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
 apk add python3
+git config --global user.email "516127941@qq.com"
+git config --global https.proxy 'socks5://gw.lan:7891'
+git config --global https.proxy 'socks5://gw.lan:7891'
+git config --global user.name "RyoLee"
+git pull
 dbl=$(cat ./dbl)
 res=$(curl -s -I -X HEAD https://cdn.jsdelivr.net/gh/wrrwrr111/pretty-derby@master/src/assert/db.json|grep content-length)
 if [ "$res"x = "x" ];then
@@ -8,17 +13,12 @@ if [ "$res"x = "x" ];then
     exit 1
 fi
 odbl=$(echo "$res"|awk '{print $2}')
-if [ "$dbl"x = $odbl"x" ];then
+if [ "$dbl"x != $odbl"x" ];then
+    sh ./build.sh
+    echo "$odbl" > ./dbl
+else
     echo "[info] no update"
-    exit 0
 fi
-git config --global user.email "516127941@qq.com"
-git config --global user.name "RyoLee"
-git config --global https.proxy 'socks5://gw.lan:7891'
-git config --global https.proxy 'socks5://gw.lan:7891'
-git pull
-sh ./build.sh
-echo "$odbl" > ./dbl
 git add ./*
 git commit -m "[autobuild] version:"$(cat version)
 git push
